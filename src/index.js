5ce4e48d26c32592503441a7d6ad5b7b5fe7ad81
Ajay Pradhan <pradhanajay14@gmail.com>
2021-08-07 20:46:19 +0530

    challenge done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Heading</h1>
+
+    <ul>
+      <li>First</li>
+      <li>Second</li>
+      <li>Third</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
